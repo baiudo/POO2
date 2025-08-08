c16a0d1350c5d04b1546d3f85d867aa8babb5af7
Rodrigo Baião de Magalhães <137411045+baiudo@users.noreply.github.com>
2025-08-08 08:25:15 -0300

    Add files via upload

---
+++ Contato.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Contato
+    {
+        public string Nome { get; set; } = nome;
+        public string NumeroTelefone { get; set; } = numeroTelefone;
+        public int Apelido { get; set; } = apelido;
+        public int QuantoMeDeve { get; set; } = quantoMeDeve;
+
+        /*public Contato(string nome, string numeroTelefone, int apelido, int quantoMeDeve)
+        {
+            Nome = nome;
+            NumeroTelefone = numeroTelefone;
+            Apelido = apelido;
+            QuantoMeDeve = quantoMeDeve;
+        }*/
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Contato c01 = new ("Vinicius", "123", "Prof", 0);
+            Contato c02 = new ("Matheus", "456", "Prof", 10);
+            Contato c03 = new ("Marta", "789", "Vizinha", 20);
+
+            AgendaTelefonica ag = new();
+
+            ag.InserirContato(c01.Nome, c01);
+            ag.InserirContato(c02.Nome, c02);
+            ag.InserirContato(c03.Nome, c03);
+
+            Console.WriteLine(ag.BuscarContato("Marta").Apelido);
+            Console.WriteLine($"Minha agenda tem {ag.QtdContatos()} contatos");
+        }
+    }
+}
