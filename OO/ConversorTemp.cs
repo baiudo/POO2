5f6dbbd38a0074b5e76d2a5090dd5bc5c670e6bb
Rodrigo Baião de Magalhães <137411045+baiudo@users.noreply.github.com>
2025-08-18 16:59:26 -0300

    Update ConversorTemp.cs

--- OO/ConversorTemp.cs
+++ OO/ConversorTemp.cs
@@ -8,6 +8,6 @@ internal class Program
 {
   private static void Main(string[] args)
   {
-    
+    Console.WriteLine("Qual operação de conversão você deseja? \n1 - Celsius -> Fahrenheit \n2 - Fahrenheit -> Celsius \n3-Celsius -> Kelvin \n4 - Kelvin -> Celsius");
   }
 }
