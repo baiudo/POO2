0c23e30ae273d8473e035e1c11063c63fb7f9a10
Rodrigo Baião de Magalhães <137411045+baiudo@users.noreply.github.com>
2025-08-20 08:37:25 -0300

    Update ConversorTemp.cs

--- OO/ConversorTemp.cs
+++ OO/ConversorTemp.cs
@@ -6,43 +6,43 @@ using System.Threading.Tasks;
 
 internal class Program
 {
-  private static void Main(string[] args)
-  {
-    while (true)
+    private static void Main(string[] args)
     {
-      Console.WriteLine("Qual operação de conversão você deseja? \n1 - Celsius -> Fahrenheit \n2 - Fahrenheit -> Celsius \n3 - Celsius -> Kelvin \n4 - Kelvin -> Celsius \n0 - Sair");
-      int conversor = int.Parse(Console.ReadLine());
-      if (conversor == 0)
-      {
-          break;
-      } else if (conversor == 1)
-      {
-          Console.WriteLine("Digite a temperatura em Celsius: ");
-          float tempC = float.Parse(Console.ReadLine());
-          float CtoF = (float)(tempC * 1.8 + 32);
-          Console.WriteLine($"{tempC:F2}ºC é o mesmo que {CtoF}ºF");
-      } else if (conversor == 2)
-      {
-          Console.WriteLine("Digite a temperatura em Fahrenheit: ");
-          float tempF = float.Parse(Console.ReadLine());
-          float FtoC = (float)((tempF - 32)/1.8);
-          Console.WriteLine($"{tempF:F2}ºF é o mesmo que {FtoC}ºC");
-      } else if (conversor == 3)
-      {
-          Console.WriteLine("Digite a temperatura em Celsius: ");
-          float tempC = float.Parse(Console.ReadLine());
-          float CtoK = (float)(tempC + 273.15);
-          Console.WriteLine($"{tempC:F2}ºF é o mesmo que {CtoK:F2}K");
-      } else if (conversor == 4)
-      {
-          Console.WriteLine("Digite a temperatua em Kelvin: ");
-          float tempK = float.Parse(Console.ReadLine());
-          float KtoC = (float)(tempK - 273.15);
-          Console.WriteLine($"{tempK:F2}K é o mesmo que {KtoC}ºC");
-      } else
-      {
-          continue;
-      }
+        while (true)
+        {
+            Console.WriteLine("Qual operação de conversão você deseja? \n1 - Celsius -> Fahrenheit \n2 - Fahrenheit -> Celsius \n3 - Celsius -> Kelvin \n4 - Kelvin -> Celsius \n0 - Sair");
+            int conversor = int.Parse(Console.ReadLine());
+            if (conversor == 0)
+            {
+                break;
+            } else if (conversor == 1)
+            {
+                Console.WriteLine("Digite a temperatura em Celsius: ");
+                float tempC = float.Parse(Console.ReadLine());
+                float CtoF = (float)(tempC * 1.8 + 32);
+                Console.WriteLine($"{tempC:F2}ºC é o mesmo que {CtoF}ºF");
+            } else if (conversor == 2)
+            {
+                Console.WriteLine("Digite a temperatura em Fahrenheit: ");
+                float tempF = float.Parse(Console.ReadLine());
+                float FtoC = (float)((tempF - 32)/1.8);
+                Console.WriteLine($"{tempF:F2}ºF é o mesmo que {FtoC}ºC");
+            } else if (conversor == 3)
+            {
+                Console.WriteLine("Digite a temperatura em Celsius: ");
+                float tempC = float.Parse(Console.ReadLine());
+                float CtoK = (float)(tempC + 273.15);
+                Console.WriteLine($"{tempC:F2}ºF é o mesmo que {CtoK:F2}K");
+            } else if (conversor == 4)
+            {
+                Console.WriteLine("Digite a temperatua em Kelvin: ");
+                float tempK = float.Parse(Console.ReadLine());
+                float KtoC = (float)(tempK - 273.15);
+                Console.WriteLine($"{tempK:F2}K é o mesmo que {KtoC}ºC");
+            } else
+            {
+                continue;
+            }
+        }
     }
-  }
 }
